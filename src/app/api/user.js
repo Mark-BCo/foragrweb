11827cc1b8317978175b8feed457ba8f6333e3f5
40110592 <mcollins49@qub.ac.uk>
2023-04-05 20:41:20 +0100

    updating api address

--- src/app/api/user.js
+++ src/app/api/user.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://localhost:3500' // base directory needs to change at deployment
+    baseURL: 'https://foragr-api.onrender.com' // base directory needs to change at deployment
 })
 
-const userRequest = axios.create({ baseURL: 'http://localhost:3500'}) // base directory needs to change at deployment
+const userRequest = axios.create({ baseURL: 'https://foragr-api.onrender.com'}) // base directory needs to change at deployment
 
 export const getUser = (userId) => userRequest.get(`/user/${userId}`);
 
